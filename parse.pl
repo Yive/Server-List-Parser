b393147ef644620d2e3c505685c6636b5eb7949c
Yive <admin@yive.me>
2021-04-04 21:36:27 -0700

    Update parse.pl

--- parse.pl
+++ parse.pl
@@ -17,7 +17,7 @@ my $agent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko)
 if($site eq "minecraftservers.org") {
     my $ua = LWP::UserAgent->new( cookie_jar => {}, agent => $agent );
     my @ips;
-    for (my $i = 1; $i <= 2186; $i++) {
+    for (my $i = 1; $i <= 1656; $i++) {
         my $req = new HTTP::Request GET => 'http://minecraftservers.org/index/'.$i;
         my $res = $ua->request($req);
         my $content = $res->content;
@@ -30,7 +30,7 @@ if($site eq "minecraftservers.org") {
     @ips = uniq(@ips);
 
     foreach my $x (@ips) {
-        my $req = new HTTP::Request GET => 'http://use.gameapis.net/mc/extra/blockedservers/check/'.$x;
+        my $req = new HTTP::Request GET => 'https://ismyserverblocked.com/check/'.$x;
         my $res = $ua->request($req);
         my $content = $res->content;
         print $content."\n";
@@ -39,7 +39,7 @@ if($site eq "minecraftservers.org") {
 } elsif($site eq "minecraft-server-list.com") {
     my $ua = LWP::UserAgent->new( cookie_jar => {}, agent => $agent );
     my @ips;
-    for (my $i = 1; $i <= 140; $i++) {
+    for (my $i = 1; $i <= 301; $i++) {
         my $req = new HTTP::Request GET => 'http://minecraft-server-list.com/page/'.$i.'/';
         my $res = $ua->request($req);
         my $content = $res->content;
@@ -52,43 +52,21 @@ if($site eq "minecraftservers.org") {
     @ips = uniq(@ips);
 
     foreach my $x (@ips) {
-        my $req = new HTTP::Request GET => 'http://use.gameapis.net/mc/extra/blockedservers/check/'.$x;
+        my $req = new HTTP::Request GET => 'https://ismyserverblocked.com/check/'.$x;
         my $res = $ua->request($req);
         my $content = $res->content;
         print $content."\n";
         Time::HiRes::sleep(0.3);
     }
 
-} elsif($site eq "minecraft-index.com") {
-    my $ua = LWP::UserAgent->new( cookie_jar => {}, agent => $agent );
-    my @ips;
-    for (my $i = 1; $i <= 959; $i++) {
-        my $req = new HTTP::Request GET => 'http://www.minecraft-index.com/page'.$i.'/';
-        my $res = $ua->request($req);
-        my $content = $res->content;
-        my @pageIps = ($content =~ /data-clipboard-text="(.*)">/g);
-        splice(@ips, 1, 0, @pageIps);
-        Time::HiRes::sleep(0.3);
-    }
-    $_ = lc for @ips;
-    s/:.*// for @ips;
-    @ips = uniq(@ips);
-
-    foreach my $x (@ips) {
-        my $req = new HTTP::Request GET => 'http://use.gameapis.net/mc/extra/blockedservers/check/'.$x;
-        my $res = $ua->request($req);
-        my $content = $res->content;
-        print $content."\n";
-        Time::HiRes::sleep(0.3);
-    }
 } elsif($site eq "minecraft-server.net") {
     my $ua = LWP::UserAgent->new( cookie_jar => {}, agent => $agent );
     my @ips;
-    for (my $i = 1; $i <= 5146; $i+=15) {
+    for (my $i = 1; $i <= 98; $i++) {
         my $req = new HTTP::Request GET => 'https://minecraft-server.net/rank/'.$i.'/in/';
         my $res = $ua->request($req);
         my $content = $res->content;
-        my @pageIps = ($content =~ /<input class="sm-form-control tleft copy-ip-trigger copyip" type="text" value="(.*)" data-out.*/g);
+        my @pageIps = ($content =~ /data-serverip="(.*)" data-versionurl=/g);
         splice(@ips, 1, 0, @pageIps);
         Time::HiRes::sleep(0.3);
     }
@@ -98,7 +76,7 @@ if($site eq "minecraftservers.org") {
     s/'.*// for @ips;
 
     foreach my $x (@ips) {
-        my $req = new HTTP::Request GET => 'http://use.gameapis.net/mc/extra/blockedservers/check/'.$x;
+        my $req = new HTTP::Request GET => 'https://ismyserverblocked.com/check/'.$x;
         my $res = $ua->request($req);
         my $content = $res->content;
         print $content."\n";
@@ -107,15 +85,15 @@ if($site eq "minecraftservers.org") {
 } elsif($site eq "topg.org") {
     my $ua = LWP::UserAgent->new( cookie_jar => {}, agent => $agent );
     my @ips;
-    for (my $i = 1; $i <= 21351; $i+=50) {
+    for (my $i = 1; $i <= 112; $i++) {
         if($i == 1) {
-            my $req = new HTTP::Request GET => 'http://topg.org/Minecraft/';
+            my $req = new HTTP::Request GET => 'https://topg.org/minecraft-servers/';
         } else {
-            my $req = new HTTP::Request GET => 'http://topg.org/Minecraft/'.$i.'/';
+            my $req = new HTTP::Request GET => 'https://topg.org/minecraft-servers/page/'.$i;
         }
         my $res = $ua->request($req);
         my $content = $res->content;
-        my @pageIps = ($content =~ /"this\.select\(\)" value="(.*)">/g);
+        my @pageIps = ($content =~ /title="Copy (.*) to Clipboard">/g);
         splice(@ips, 1, 0, @pageIps);
         Time::HiRes::sleep(0.3);
     }
@@ -124,18 +102,16 @@ if($site eq "minecraftservers.org") {
     @ips = uniq(@ips);
 
     foreach my $x (@ips) {
-        my $req = new HTTP::Request GET => 'http://use.gameapis.net/mc/extra/blockedservers/check/'.$x;
+        my $req = new HTTP::Request GET => 'https://ismyserverblocked.com/check/'.$x;
         my $res = $ua->request($req);
         my $content = $res->content;
         print $content."\n";
         Time::HiRes::sleep(0.3);
     }
-} elsif($site eq "planetminecraft.com") {
-
 } elsif($site eq "minecraft-mp.com") {
     my $ua = LWP::UserAgent->new( cookie_jar => {}, agent => $agent );
     my @ips;
-    for (my $i = 1; $i <= 665; $i++) {
+    for (my $i = 1; $i <= 566; $i++) {
         my $req = new HTTP::Request GET => 'http://minecraft-mp.com/servers/list/'.$i.'/';
         my $res = $ua->request($req);
         my $content = $res->content;
@@ -148,7 +124,7 @@ if($site eq "minecraftservers.org") {
     @ips = uniq(@ips);
 
     foreach my $x (@ips) {
-        my $req = new HTTP::Request GET => 'http://use.gameapis.net/mc/extra/blockedservers/check/'.$x;
+        my $req = new HTTP::Request GET => 'https://ismyserverblocked.com/check/'.$x;
         my $res = $ua->request($req);
         my $content = $res->content;
         print $content."\n";
@@ -157,11 +133,11 @@ if($site eq "minecraftservers.org") {
 } elsif($site eq "minelist.net") {
     my $ua = LWP::UserAgent->new( cookie_jar => {}, agent => $agent );
     my @ips;
-    for (my $i = 1; $i <= 76; $i++) {
-        my $req = new HTTP::Request GET => 'http://minelist.net/'.$i;
+    for (my $i = 1; $i <= 200; $i++) {
+        my $req = new HTTP::Request GET => 'http://minelist.net/page/'.$i;
         my $res = $ua->request($req);
         my $content = $res->content;
-        my @pageIps = ($content =~ /"this\.select\(\)" value="(.*)">/g);
+        my @pageIps = ($content =~ /"value="(.*)" onclick="if /g);
         splice(@ips, 1, 0, @pageIps);
         Time::HiRes::sleep(0.3);
     }
@@ -170,54 +146,7 @@ if($site eq "minecraftservers.org") {
     @ips = uniq(@ips);
 
     foreach my $x (@ips) {
-        my $req = new HTTP::Request GET => 'http://use.gameapis.net/mc/extra/blockedservers/check/'.$x;
-        my $res = $ua->request($req);
-        my $content = $res->content;
-        print $content."\n";
-        Time::HiRes::sleep(0.3);
-    }
-} elsif($site eq "minecraftserverlist.eu") {
-
-} elsif($site eq "minecraftforum.net") {
-    my $ua = LWP::UserAgent->new( cookie_jar => {}, agent => $agent );
-    my @ips;
-    for (my $i = 1; $i <= 809; $i++) {
-        my $req = new HTTP::Request GET => 'http://www.minecraftforum.net/servers?page='.$i;
-        my $res = $ua->request($req);
-        my $content = $res->content;
-        my @pageIps = ($content =~ /<span id="host-name-.*">(.*)<\/span>/g);
-        splice(@ips, 1, 0, @pageIps);
-        Time::HiRes::sleep(0.3);
-    }
-    $_ = lc for @ips;
-    s/:.*// for @ips;
-    @ips = uniq(@ips);
-
-    foreach my $x (@ips) {
-        my $req = new HTTP::Request GET => 'http://use.gameapis.net/mc/extra/blockedservers/check/'.$x;
-        my $res = $ua->request($req);
-        my $content = $res->content;
-        print $content."\n";
-        Time::HiRes::sleep(0.3);
-    }
-} elsif($site eq "minepick.com") {
-    my $ua = LWP::UserAgent->new( cookie_jar => {}, agent => $agent );
-    my @ips;
-    for (my $i = 1; $i <= 981; $i++) {
-        my $req = new HTTP::Request GET => 'http://www.minepick.com/page/'.$i.'/';
-        my $res = $ua->request($req);
-        my $content = $res->content;
-        my @pageIps = ($content =~ /value='(.*)'.*ip-box'/g);
-        splice(@ips, 1, 0, @pageIps);
-        Time::HiRes::sleep(0.3);
-    }
-    $_ = lc for @ips;
-    s/:.*// for @ips;
-    @ips = uniq(@ips);
-    s/'.*// for @ips;
-
-    foreach my $x (@ips) {
-        my $req = new HTTP::Request GET => 'http://use.gameapis.net/mc/extra/blockedservers/check/'.$x;
+        my $req = new HTTP::Request GET => 'https://ismyserverblocked.com/check/'.$x;
         my $res = $ua->request($req);
         my $content = $res->content;
         print $content."\n";
@@ -226,11 +155,11 @@ if($site eq "minecraftservers.org") {
 } elsif($site eq "mc-servers.com") {
     my $ua = LWP::UserAgent->new( cookie_jar => {}, agent => $agent );
     my @ips;
-    for (my $i = 1; $i <= 107; $i++) {
+    for (my $i = 1; $i <= 262; $i++) {
         my $req = new HTTP::Request GET => 'https://mc-servers.com/page/'.$i.'/';
         my $res = $ua->request($req);
         my $content = $res->content;
-        my @pageIps = ($content =~ /<input id="copyip" class="ipcopy" readonly="readonly" value="(.*)">/g);
+        my @pageIps = ($content =~ /data-clipboard-text="(.*)" data-position="/g);
         splice(@ips, 1, 0, @pageIps);
         Time::HiRes::sleep(0.3);
     }
@@ -240,26 +169,16 @@ if($site eq "minecraftservers.org") {
     s/'.*// for @ips;
 
     foreach my $x (@ips) {
-        my $req = new HTTP::Request GET => 'http://use.gameapis.net/mc/extra/blockedservers/check/'.$x;
+        my $req = new HTTP::Request GET => 'https://ismyserverblocked.com/check/'.$x;
         my $res = $ua->request($req);
         my $content = $res->content;
         print $content."\n";
         Time::HiRes::sleep(0.3);
     }
-} elsif($site eq "mineservers.com") {
-
-} elsif($site eq "mcserverlist.com") {
-
-} elsif($site eq "minecraft-list.com") {
-
-} elsif($site eq "mcserverstatus.com") {
-
-} elsif($site eq "minecraft-servers-list.net") {
-
 } elsif($site eq "serveurs-minecraft.com") {
     my $ua = LWP::UserAgent->new( cookie_jar => {}, agent => $agent );
     my @ips;
-    for (my $i = 1; $i <= 613; $i++) {
+    for (my $i = 1; $i <= 788; $i++) {
         my $req = new HTTP::Request GET => 'https://www.serveurs-minecraft.com/?page='.$i;
         my $res = $ua->request($req);
         my $content = $res->content;
@@ -273,30 +192,7 @@ if($site eq "minecraftservers.org") {
     s/'.*// for @ips;
 
     foreach my $x (@ips) {
-        my $req = new HTTP::Request GET => 'http://use.gameapis.net/mc/extra/blockedservers/check/'.$x;
-        my $res = $ua->request($req);
-        my $content = $res->content;
-        print $content."\n";
-        Time::HiRes::sleep(0.3);
-    }
-} elsif($site eq "minecraftpeservers.org") {
-    # Experimenting with PE servers. Doubt there will be a blacklist involving PE servers, but better safe than sorry.
-    my $ua = LWP::UserAgent->new( cookie_jar => {}, agent => $agent );
-    my @ips;
-    for (my $i = 1; $i <= 53; $i++) {
-        my $req = new HTTP::Request GET => 'http://minecraftpeservers.org/index/'.$i;
-        my $res = $ua->request($req);
-        my $content = $res->content;
-        my @pageIps = ($content =~ /<p><span class="icon ip"><\/span>(.*)<\/p>/g);
-        splice(@ips, 1, 0, @pageIps);
-        Time::HiRes::sleep(0.3);
-    }
-    $_ = lc for @ips;
-    s/:.*// for @ips;
-    @ips = uniq(@ips);
-
-    foreach my $x (@ips) {
-        my $req = new HTTP::Request GET => 'http://use.gameapis.net/mc/extra/blockedservers/check/'.$x;
+        my $req = new HTTP::Request GET => 'https://ismyserverblocked.com/check/'.$x;
         my $res = $ua->request($req);
         my $content = $res->content;
         print $content."\n";
